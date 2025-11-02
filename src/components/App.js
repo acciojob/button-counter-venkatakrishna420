e940bf0fc07262247cea4cd6f128ba8cdfe91182
KAKARLA VENKATA KRISHNA <kakarlavenkatakrishna9@gmail.com>
2025-11-02 12:36:56 +0530

    Implement Render component with click counter
    Added a Render component to track button clicks.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+
 
 const App = () => {
   return (
     <div>
+     <Render />
         {/* Do not remove the main div */}
     </div>
   )
 }
 
 export default App
+
+ function Render() {
+  const[count,setState]=useState(0)  
+  return (
+    <div>
+      <p>Button clicked {count} times</p>      
+      <button onClick={()=>setState(count+1)}>Click me</button>
+    </div>
+    
+  );
+}
